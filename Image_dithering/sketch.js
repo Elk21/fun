944a3702362079954d899d3fcdf5183f32cb9e08
elk21 <danivlad6@gmail.com>
2018-04-26 19:53:15 +0300

    added menu

--- Image_dithering/sketch.js
+++ Image_dithering/sketch.js
@@ -1,34 +1,62 @@
 /* 
   Realisation of Floyd–Steinberg dithering algorithm
-
   https://en.wikipedia.org/wiki/Floyd–Steinberg_dithering
 
+  Create image of dots
+
   TODO:
-  1. Create menu
   2. Load img from user
 */
 
 
-let img;
+let IMG;
 let newImg;
 
-// Number of colors = scl ^ 3
-const scl = 2;
+let colorsSlider;
+let imageSizeSlider;
+
+let scl = 1; // Number of colors in image = scl ^ 3
+let imgSize = 100;
+let grey = false;
 
 function preload() {
-  img = loadImage('https://c1.staticflickr.com/1/176/473792114_839a6d66ac_z.jpg?zz=1');
-  //img = loadImage('https://pbs.twimg.com/profile_images/435107609767858177/kaPOalZy.jpeg');
-  
+  IMG = loadImage('https://c1.staticflickr.com/1/176/473792114_839a6d66ac_z.jpg?zz=1');
 }
 
 function setup() {
   createCanvas(1000, 500);
   background(51);
 
-  //img.filter(GRAY);
-  img.resize(500, 500);
+  // Create slider for picking number of colors
+  colorsDiv = select('#colorsDiv');
+  colorsSlider = createSlider(1, 10, 1, 1);
+  colorsDiv.child(colorsSlider);
 
-  image(img, 0, 0, img.width, img.height);
+  // Slider for image size
+  imageSizeDiv = select('#imageSizeDiv');
+  imageSizeSlider = createSlider(10, 500, 100, 10);
+  imageSizeDiv.child(imageSizeSlider);
+
+  select('#checkbox').changed(() => grey = !grey);
+}
+
+function draw() {
+  background(51);
+  scl = colorsSlider.value();
+  imgSize = imageSizeSlider.value();
+  dither();
+}
+
+function dither() {
+  // Create new source img
+  let img = createImage(imgSize, imgSize);
+  img.copy(IMG, 0, 0, IMG.width, IMG.height, 0, 0, imgSize, imgSize)
+  img.resize(imgSize, imgSize);
+
+  // Grey scale
+  if (grey) img.filter(GRAY);
+
+  image(img, 0, 0, 500, 500);
   newImg = createImage(img.width, img.height);
 
   img.loadPixels();
@@ -36,28 +64,33 @@ function setup() {
 
   for (let y = 0; y < img.height * 4 - 4; y += 4) {
     for (let x = 4; x < img.width * 4 - 4; x += 4) {
-      let i = index(x, y);
+      let i = index(x, y, img.width);
 
+      // Get rgb color of each pixel
       let oldR = img.pixels[i + 0];
       let oldG = img.pixels[i + 1];
       let oldB = img.pixels[i + 2];
       let oldA = img.pixels[i + 3];
 
+      // Calculate new color
       let newR = round(scl * oldR / 255) * (255 / scl);
       let newG = round(scl * oldG / 255) * (255 / scl);
       let newB = round(scl * oldB / 255) * (255 / scl);
 
+      // Set pixel color to new image
       newImg.pixels[i + 0] = newR;
       newImg.pixels[i + 1] = newG;
       newImg.pixels[i + 2] = newB;
       newImg.pixels[i + 3] = oldA;
 
+      // Calculate error
       let errR = oldR - newR;
       let errG = oldG - newG;
       let errB = oldB - newB;
 
 
-      let ii = index(x + 4, y);
+      // Set color to neighbours from left-top to right-bottom
+      let ii = index(x + 4, y, img.width);
       let r = oldR + errR * 7 / 16.0;
       let g = oldG + errG * 7 / 16.0;
       let b = oldB + errB * 7 / 16.0;
@@ -66,7 +99,7 @@ function setup() {
       newImg.pixels[ii + 2] = b;
       newImg.pixels[ii + 3] = oldA;
 
-      ii = index(x - 4, y + 4);
+      ii = index(x - 4, y + 4, img.width);
       r = oldR + errR * 3 / 16.0;
       g = oldG + errG * 3 / 16.0;
       b = oldB + errB * 3 / 16.0;
@@ -75,7 +108,7 @@ function setup() {
       newImg.pixels[ii + 2] = b;
       newImg.pixels[ii + 3] = oldA;
 
-      ii = index(x, y + 4);
+      ii = index(x, y + 4, img.width);
       r = oldR + errR * 5 / 16.0;
       g = oldG + errG * 5 / 16.0;
       b = oldB + errB * 5 / 16.0;
@@ -84,7 +117,7 @@ function setup() {
       newImg.pixels[ii + 2] = b;
       newImg.pixels[ii + 3] = oldA;
 
-      ii = index(x + 4, y + 4);
+      ii = index(x + 4, y + 4, img.width);
       r = oldR + errR * 1 / 16.0;
       g = oldG + errG * 1 / 16.0;
       b = oldB + errB * 1 / 16.0;
@@ -98,9 +131,10 @@ function setup() {
   newImg.updatePixels();
   img.updatePixels();
 
-  image(newImg, img.width, 0, img.width, img.height);
+  image(newImg, 500, 0, 500, 500);
 }
 
-function index(x, y) {
-  return x + y * img.width;
+// Calculates 1D index from 2D indexes
+function index(x, y, w) {
+  return x + y * w;
 }
